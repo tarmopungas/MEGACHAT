93039d0566806ac7d033d904eecedd405c1eca2a
joosep <joosep.naks@mail.ee>
2020-03-28 02:37:34 +0200

    lisasin kuulajasse selle, et ta hoiab klienti enda küljes mitme requesti jooksul ja et kindlasti alguses loetakse ka request sisse ja pärast saadetakse väljund ja errorcode tagasi

--- src/Kuulaja.java
+++ src/Kuulaja.java
@@ -13,19 +13,37 @@ public class Kuulaja implements Runnable{
 
     @Override
     public void run() {
+        // ootab uusi kliente
         while (true) {
             try (Socket socket = ss.accept();
                  InputStream in = socket.getInputStream();
                  OutputStream out = socket.getOutputStream();
                  DataInputStream din = new DataInputStream(in);
-                 DataOutputStream dout = new DataOutputStream(out)) {
-                int requestType = din.read();
-                switch (requestType) {
-                    // siin kutsutakse handlereid välja caseidega
-                    default:
-                        System.out.println("Midagi on valesti, lugesin request typei: " + requestType);
-                        // kliendile saadetakse "mittedefineeritud request type"
-                        dout.write(1);
+                 DataOutputStream dout = new DataOutputStream(out)){
+                // ootab uusi requeste
+                while (true) {
+                    int requestType = din.read();
+                    int requestSize = din.read();
+                    byte[] request = new byte[requestSize];
+                    din.read(request, 0, requestSize);
+                    Integer errorCode = 0;
+                    byte[] output = null;
+                    switch (requestType) {
+                        // siin kutsutakse handlereid välja caseidega
+                        case 1:
+                            RegisterHandler registerHandler = new RegisterHandler(errorCode);
+                            output = registerHandler.handle(request);
+                            break;
+                        default:
+                            System.out.println("Midagi on valesti, lugesin request typei: " + requestType);
+                            // kliendile saadetakse "mittedefineeritud request type"
+                            errorCode = 1;
+                    }
+                    dout.write(errorCode);
+                    if (errorCode == 0 && output != null) {
+                        dout.write(output.length);
+                        dout.write(output);
+                    }
                 }
             } catch (IOException e) {
                 throw new RuntimeException("Pordilugemine ei tööta");
